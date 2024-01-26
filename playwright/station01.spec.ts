243d5fd4750b01c46e10ccef568317cdb2d736d2
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-01-27 03:00:23 +0900

    問題文を変更してみた

--- playwright/station01.spec.ts
+++ playwright/station01.spec.ts
@@ -8,8 +8,12 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/station1.html')
 })
 
+// test('<ul>が存在する', async ({ page }) => {
+//   await expect(page).toHaveSelector('ul')
+// })
+
 test('<ul>が存在する', async ({ page }) => {
-  await expect(page).toHaveSelector('ul')
+  await expect(page).toHaveSelector('ul', { timeout: 10000 }) // 例: 10秒まで待機
 })
 
 test('<li>で囲われた「りんご」、「ばなな」、「ぶどう」がそれぞれ表示されている', async ({

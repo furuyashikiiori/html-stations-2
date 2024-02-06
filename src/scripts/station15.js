b20bf584587b397f176ac8323dc4ec90703ac848
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-02-07 01:31:47 +0900

    おわったよ

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -2,8 +2,11 @@ function displayList() {
   // const fruitElements = document.getElementById("fruits");
   // const ul = document.createElement("ul");
 
-  const fruitsDiv = document.getElementById("fruits");
-  fruitsDiv.innerHTML = `<ul><li>${fruitsDiv.innerHTML
-    .replace(/<p>/g, "<li>")
-    .replace(/<\/p>/g, "</li>")}</ul>`;
+  // const fruitsDiv = document.getElementById("fruits");
+  // fruitsDiv.innerHTML = `<ul><li>${fruitsDiv.innerHTML
+  //   .replace(/<p>/g, "<li>")
+  //   .replace(/<\/p>/g, "</li>")}</ul>`;
+
+  const fruits = document.getElementById("fruits");
+  fruits.innerHTML = "<ul><li>リンゴ<li><li>イチゴ<li><li>ブドウ<li><ul>";
 }
